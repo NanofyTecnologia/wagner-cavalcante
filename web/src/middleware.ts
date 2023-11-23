4f2cb05353aa1363fe3c72df11d117f309287556
Vinicius <vinicezar800@hotmail.com>
2023-11-23 16:57:33 -0300

    fix: error to loading tinymce

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export default async function middleware(req: NextRequest) {
   const token = req.cookies.get('token')?.value
+
   if (!token) {
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_WEB_URL}/login`)
   }
