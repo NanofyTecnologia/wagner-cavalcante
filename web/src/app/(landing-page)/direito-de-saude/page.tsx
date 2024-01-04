9fdd4fb43d6c9201e642a6fcb1f064223c7ace45
Vinicius <vinicezar800@hotmail.com>
2024-01-04 09:46:09 -0300

    feat: updated services section

--- web/src/app/(landing-page)/direito-de-saude/page.tsx
+++ web/src/app/(landing-page)/direito-de-saude/page.tsx
@@ -3,10 +3,16 @@ import Image from 'next/image'
 
 import PhotoSamuel from '@/assets/images/samuel.jpg'
 
-import { GoLaw } from 'react-icons/go'
-import { SiGoogledocs } from 'react-icons/si'
-import { RiHealthBookLine } from 'react-icons/ri'
-import { MdHealthAndSafety } from 'react-icons/md'
+import { BsHeartPulse, BsHeartPulseFill } from 'react-icons/bs'
+import {
+  FaHome,
+  FaBriefcaseMedical,
+  FaHandHoldingUsd,
+  FaFileMedical,
+  FaHospitalAlt,
+  FaRegMoneyBillAlt,
+} from 'react-icons/fa'
+import { FaSackDollar } from 'react-icons/fa6'
 
 export default function LandingPage() {
   return (
@@ -14,9 +20,9 @@ export default function LandingPage() {
       <div className="relative bg-[url(../assets/images/assinando-papeis.jpg)] bg-cover">
         <div className="bg-black bg-opacity-60 py-6">
           <main className="mx-auto max-w-7xl py-32">
-            <div className="grid grid-cols-1 items-center px-12 sm:grid-cols-2">
+            <div className="px-12 text-center">
               <div className="text-secondary">
-                <h1 className="font-garamond text-xl font-semibold uppercase">
+                <h1 className="font-garamond text-4xl font-semibold uppercase">
                   Monte & Cavalcante
                 </h1>
 
@@ -24,15 +30,15 @@ export default function LandingPage() {
                   Advocacia
                 </h2>
 
-                <p className="mt-4 text-3xl">
+                <p className="mt-4 text-4xl">
                   Advogado Especializado em Direito de Saúde
                 </p>
 
-                <div className="flex">
+                <div className="flex justify-center">
                   <Link
                     prefetch={false}
                     target="_blank"
-                    href="https://api.whatsapp.com/send/?phone=558296680662&text&type=phone_number&app_absent=0"
+                    href="https://api.whatsapp.com/send/?phone=5582999355766&text&type=phone_number&app_absent=0"
                     className="mt-6 block rounded-md bg-primary px-12 py-4 text-sm font-semibold uppercase text-white"
                   >
                     Fale Com o Advogado Agora
@@ -44,14 +50,104 @@ export default function LandingPage() {
         </div>
       </div>
 
-      <section className="bg-neutral-900">
+      <section className="bg-white">
         <div className="mx-auto max-w-7xl px-12 py-8">
           <h2 className="mb-8 text-center text-xl font-semibold uppercase text-secondary">
             Nossos serviços
           </h2>
 
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-            <div className="rounded-md border border-primary p-6">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-3">
+            <div className="flex flex-col items-center">
+              <div className="flex items-center justify-center rounded-full bg-black p-4 text-4xl text-white">
+                <BsHeartPulse />
+              </div>
+
+              <p className="mt-2 text-center text-xl font-bold text-neutral-700">
+                Negativa de Plano de Saúde
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <div className="flex items-center justify-center rounded-full bg-black p-4 text-4xl text-white">
+                <FaRegMoneyBillAlt />
+              </div>
+
+              <p className="mt-2 text-center  text-xl font-bold text-neutral-700">
+                Reajuste Abusivo
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <div className="flex items-center justify-center rounded-full bg-black p-4 text-4xl text-white">
+                <FaHome />
+              </div>
+
+              <p className="mt-2 text-center text-xl font-bold text-neutral-700">
+                Home Care / Internação <br /> Domiciliar
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <div className="flex items-center justify-center rounded-full bg-black p-4 text-4xl text-white">
+                <FaBriefcaseMedical />
+              </div>
+
+              <p className="mt-2 text-center text-xl font-bold text-neutral-700">
+                Indenização Por Erro Médico <br /> / Odontológico
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <div className="flex items-center justify-center rounded-full bg-black p-4 text-4xl text-white">
+                <BsHeartPulseFill />
+              </div>
+
+              <p className="mt-2 text-center text-xl font-bold text-neutral-700">
+                Indenização Por Dano <br /> Estético
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <div className="flex items-center justify-center rounded-full bg-black p-4 text-4xl text-white">
+                <FaHandHoldingUsd />
+              </div>
+
+              <p className="mt-2 text-center text-xl font-bold text-neutral-700">
+                Medicação de Alto Custo
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <div className="flex items-center justify-center rounded-full bg-black p-4 text-4xl text-white">
+                <FaSackDollar />
+              </div>
+
+              <p className="mt-2 text-center text-xl font-bold text-neutral-700">
+                Reebolso de Despesas <br /> Médicas
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <div className="flex items-center justify-center rounded-full bg-black p-4 text-4xl text-white">
+                <FaFileMedical />
+              </div>
+
+              <p className="mt-2 text-center text-xl font-bold text-neutral-700">
+                Cancelamento Indevido do <br /> Plano de Saúde
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <div className="flex items-center justify-center rounded-full bg-black p-4 text-4xl text-white">
+                <FaHospitalAlt />
+              </div>
+
+              <p className="mt-2 text-center text-xl font-bold text-neutral-700">
+                Demora Na Autorização de <br /> Cirurgias
+              </p>
+            </div>
+
+            {/* <div className="rounded-md border border-primary p-6">
               <RiHealthBookLine className="mx-auto text-5xl text-secondary" />
 
               <h3 className="my-4 text-center text-xl font-semibold text-primary">
@@ -112,7 +208,7 @@ export default function LandingPage() {
                 ser respeitado. Por conta disso, o suporte de um advogado
                 especializado se faz necessário
               </p>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
@@ -120,27 +216,37 @@ export default function LandingPage() {
       <section>
         <div className="mx-auto max-w-7xl px-12 py-8">
           <h2 className="text-center text-xl font-semibold uppercase text-black">
-            Sobre nós
+            Quem é o Advogado?
           </h2>
 
           <div className="my-6 grid grid-cols-1 gap-12 sm:grid-cols-2">
-            <div className="h-96 rounded-md bg-neutral-500">
+            <div className="h-[460px] rounded-md bg-neutral-500">
               <Image
                 src={PhotoSamuel}
                 alt="doutor samuel monte"
-                className="h-full w-full rounded-md object-cover object-[0%_35%] "
+                className="h-full w-full rounded-md object-cover object-[0%_40%] "
               />
             </div>
 
-            <div className="space-y-4">
+            <div className="space-y-4 text-justify text-2xl">
               <p>
                 Samuel Monte, estudei na faculdade de Direito da Universidade
-                Federal de Alagoas, sou especialista em Direito Público e em
-                Direito Médico e da Saúde, vice presidente da comissão de
-                Direito Médico e da Saúde da OAB AL, advogado desde 2018, com
-                atuação focada em na relação médico/paciente quanto contra
-                planos de saúde, sempre com foco em ajudar aqueles que precisam
-                de tutelas em saúde.
+                Federal de Alagoas, especialista em Direito Público e em Direito
+                Médico e da Saúde.
+              </p>
+
+              <p>
+                Advogado desde 2018, atuo como vice presidente da comissão de
+                Direito Médico e da Saúde da OAB AL, com foco na relação dos
+                segurados contra planos de saúde, sempre dando o meu melhor para
+                ajudar aqueles que precisam de proteção quando o assunto é
+                direito de saúde.
+              </p>
+
+              <p>
+                Com atendimento personalizado, analiso caso a caso, visando
+                entender todos os pontos e repassar ao cliente a melhor solução
+                possível.
               </p>
             </div>
           </div>
@@ -149,7 +255,7 @@ export default function LandingPage() {
             <Link
               prefetch={false}
               target="_blank"
-              href="https://api.whatsapp.com/send/?phone=558296680662&text&type=phone_number&app_absent=0"
+              href="https://api.whatsapp.com/send/?phone=5582999355766&text&type=phone_number&app_absent=0"
               className="mt-6 block rounded-md bg-primary px-12 py-4 text-sm font-semibold uppercase text-white"
             >
               Fale Com o Advogado Agora
