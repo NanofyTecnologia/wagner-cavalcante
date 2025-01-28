ff31705b242c5839dbc08b67db0ed2897ed4c086
Nanofy Tecnologia <145067683+NanofyTecnologia@users.noreply.github.com>
2025-01-27 23:30:20 -0300

    altera pagina com status em construcao

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -11,7 +11,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: 'Ops! Página em construção...',
-  description: 'by Nanofy Tecnologia Ltda',
+  description: 'Desenvolvido por Nanofy Tecnologia Ltda.',
 }
 
 export default function RootLayout({
